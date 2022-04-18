c953e71f16fe8c1d473ff41412ac97bfc10cb0ca
ckpd3453 <98898121+ckpd3453@users.noreply.github.com>
2022-04-18 15:26:35 +0530

    modified

--- src/main/java/Measurement.java
+++ src/main/java/Measurement.java
@@ -3,7 +3,7 @@ public class Measurement {
 
 	public int comparision(int feet) {
 		if(feet == 1) {
-            int inch = 12;
+            int inch = feet*12;
             return inch;
         }
         return 0;
